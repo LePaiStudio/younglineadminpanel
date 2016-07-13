7c05f277439a4cbef10c38a45e1c661274158be1
Jerry Lu <lyd@lep.ai>
2016-07-13 13:48:56 +0800

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,56 @@
 import React, { PropTypes } from 'react';
-import { Router, Route, IndexRoute, Link } from 'react-router';
 import App from '../components/App';
 import NotFound from '../components/NotFound';
+import { Menu, Breadcrumb, Icon } from 'antd';
+const SubMenu = Menu.SubMenu;
 
 const Routes = ({ history }) =>
-  <Router history={history}>
-    <Route path="/" component={App} />
-    <Route path="/actived" component={App} />
-    <Route path="/completed" component={App} />
-    <Route path="*" component={NotFound}/>
-  </Router>;
+    <div className="ant-layout-aside">
+      <aside className="ant-layout-sider">
+        <div className="ant-layout-logo"></div>
+	        <Menu mode="inline" theme="dark"
+	          defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']}>
+	          <SubMenu key="sub1" title={<span><Icon type="user" />导航一</span>}>
+	          	<Menu.Item key="1">选项1</Menu.Item>
+	            <Menu.Item key="2">选项2</Menu.Item>
+	            <Menu.Item key="3">选项3</Menu.Item>
+	            <Menu.Item key="4">选项4</Menu.Item>
+	          </SubMenu>
+	          <SubMenu key="sub2" title={<span><Icon type="laptop" />导航二</span>}>
+	            <Menu.Item key="5">选项5</Menu.Item>
+	            <Menu.Item key="6">选项6</Menu.Item>
+	            <Menu.Item key="7">选项7</Menu.Item>
+	            <Menu.Item key="8">选项8</Menu.Item>
+	          </SubMenu>
+	          <SubMenu key="sub3" title={<span><Icon type="notification" />导航三</span>}>
+	            <Menu.Item key="9">选项9</Menu.Item>
+	            <Menu.Item key="10">选项10</Menu.Item>
+	            <Menu.Item key="11">选项11</Menu.Item>
+	            <Menu.Item key="12">选项12</Menu.Item>
+	          </SubMenu>
+	        </Menu>
+      </aside>
+      <div className="ant-layout-main">
+        <div className="ant-layout-header"></div>
+        <div className="ant-layout-breadcrumb">
+          <Breadcrumb>
+            <Breadcrumb.Item>首页</Breadcrumb.Item>
+            <Breadcrumb.Item>应用列表</Breadcrumb.Item>
+            <Breadcrumb.Item>某应用</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+        <div className="ant-layout-container">
+          <div className="ant-layout-content">
+            <div style={{ height: 590 }}>
+              内容区域
+            </div>
+          </div>
+        </div>
+        <div className="ant-layout-footer">
+        Ant Design 版权所有 © 2015 由蚂蚁金服体验技术部支持
+        </div>
+      </div>
+    </div>;
 
 Routes.propTypes = {
   history: PropTypes.any,
